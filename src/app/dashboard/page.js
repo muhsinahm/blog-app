e076a1db4217b91b863f6ee14a16e189a9570630
muhsinahm <muhsingcu@gmail.com>
2025-02-17 03:30:37 +0500

    file colocation

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,5 @@
+import LineChart from "./line-chart";
+
+export default function BarChart() {
+  return LineChart();
+}
---
+++ src/app/products/[productId]/page.js
@@ -0,0 +1,3 @@
+export default function ProductDetails({ params }) {
+  return <h1>Details About Product {params.productId}</h1>;
+}
